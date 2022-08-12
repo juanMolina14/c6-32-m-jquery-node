17c9bec0f3abab43bae3d154652c48b20f246a4e
Agustin Boccio <aboccio@hotmail.com>
2022-08-12 17:04:17 +0200

    Project first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={<Navbar />}
+        />
+        <Route
+          path="/search"
+          element={<Navbar />}
+        />
+        <Route
+          path="/report"
+          element={<Navbar />}
+        />
+        <Route
+          path="/adopt"
+          element={<Navbar />}
+        />
+        <Route
+          path="/login"
+          element={<Navbar />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navbar.css';
+
+export default function Navbar(){
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Contacto</li>
+            <li>Q somos</li>
+            <li>Donde vamos</li>
+        </ul>
+        )
+}
