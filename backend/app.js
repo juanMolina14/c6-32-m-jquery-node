a88501bbb64aee4cc40c3bbf3741bee81c2bda4d
Agustin Boccio <aboccio@hotmail.com>
2022-08-19 09:16:28 +0200

    Router set up

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
+const router = require('./router/mainRouter');
 
 app.listen(process.env.PORT || 3000, () => console.log('Server running'));
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.use('/', router);
