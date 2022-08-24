63a823a145c724d25de9b5ea777bb030e80c73c6
juanMolina14 <juan.molina159@gmail.com>
2022-08-23 23:07:42 -0300

    Correcion parrafos y titulo

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import CardsMascotas from '../CardsMascotas/CardsMascotas'
 import Navbar from '../navbar/Navbar'
 import Nosotros from '../Nosotros/Nosotros'
+import { NavLink } from 'react-router-dom'
 
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar />
       <Nosotros/>
       <CardsMascotas/>
-    </div>
+      <figure className="text-center">
+                <h1>¿Encontraste o Perdiste?</h1>
+                <blockquote className="blockquote">
+                    <p className="mb-0">Publica una nueva busqueda en nuestro Portal</p>
+                </blockquote>
+                <br></br>
+                <button type="button" className="btn btn-danger"><NavLink className="btn-Reportar" to="/Report">Reportar</NavLink></button>
+      </figure>
+    </>
   )
 }
 
