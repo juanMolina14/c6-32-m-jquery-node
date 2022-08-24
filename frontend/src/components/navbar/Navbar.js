f2b9d61206d89bd725a39909e62553135ab0a224
juanMolina14 <juan.molina159@gmail.com>
2022-08-23 23:09:46 -0300

    Agregue icono avatar para acceder a profile

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -2,28 +2,31 @@ import React from "react";
 import './Navbar.css';
 import logo from '../img/logo.png'
 import { NavLink } from 'react-router-dom'
+import Headroom from "react-headroom";
+import Avatar from '@mui/material/Avatar'
+
 
 
 export default function Navbar() {
     return (
         <>
+        <Headroom>
+          
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/home"><img src={logo} alt="logo" width="150px"/></a>
+                <div className="container-fluid ">
+                    <a className="navbar-brand" href="/home"><img src={logo} alt="logo" width="200px"/></a>
 
-                    <form className="d-flex">
+                    <div className="">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarColor03">
-                        <ul className="navbar-nav me-auto">
-                            <li className="nav-item">
-                                <NavLink to="/home" className="nav-link active">Home
-                                    <span className="visually-hidden">(current)</span>
-                                </NavLink>
+                        <ul className="navbar-nav align-items-center">
+                            <li className="nav-item align-items-center">
+                                <NavLink to="/home" className="nav-link">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/search">Search</a>
+                                <NavLink to="/search" className="nav-link" >Search</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to="/report" className="nav-link" >Report</NavLink>
@@ -34,16 +37,28 @@ export default function Navbar() {
                             <li className="nav-item">
                                 <NavLink to="/login" className="nav-link">Login</NavLink>
                             </li>
+                            <li className="nav-item">
+                                <NavLink to="/profile" className="nav-link">
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+                                    sx={{ width: 50, height: 50 }}
+                                />
+                                </NavLink>
+                               
+
+                            </li>
                         </ul>
                     </div>
                     
                         
-                    </form>
+                    </div>
                 </div>
 
 
                
             </nav>
+        </Headroom>
         </>
     )
 }
