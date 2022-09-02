8d33a0c40208743c68cfaf043fc229841fdf7400
Agustin Boccio <aboccio@hotmail.com>
2022-09-02 17:17:54 +0200

    Login process, saving user logged in localStorage. Redirect to home if succeded, print error if failed

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,79 +1,138 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import { CssVarsProvider } from '@mui/joy/styles';
 import Sheet from '@mui/joy/Sheet';
 import Typography from '@mui/joy/Typography';
 import TextField from '@mui/joy/TextField';
 import Button from '@mui/joy/Button';
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Navbar from '../navbar/Navbar'
+import Footer from '../Footer/Footer'
+import { useForm } from "react-hook-form";
 import './login.css'
 
 const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [loginError, setLoginError] = useState([]);
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        localStorage.setItem('userData', JSON.stringify(userData));
+    }, [userData]);
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+        defaultValues:{
+        //shelter: false,
+    }
+    });
+
+    const onSubmit = (data) =>  {
+        
+        console.log(`JSON STRINGIFY #1 ${JSON.stringify(data)}`)
+
+        const validation = fetch("http://localhost:3030/login", {
+            method: "POST",
+            //   credentials: 'include',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: data.email,
+                password: data.password,
+            }),
+        })
+        .then( response => {
+                if (!response.ok) { setLoginError(response) };
+                console.log(response);
+                // response.json();
+                // setUserData(response.json());
+
+                return response.json();
+
+                // console.log(JSON.stringify(data.json()));
+                // sessionStorage.setItem('userData', JSON.stringify(data.json()));
+                // alert(JSON.stringify(data))
+                // alert(hasError);
+            }
+        )
+
+        const saveUser = async () => {
+            const userLogged = await validation;
+            setUserData(userLogged);
+            if (userLogged !== undefined){
+                return navigate('/');
+            }
+          };
+
+        saveUser();
+    };
+
     return (
-        <CssVarsProvider className="Ubicacion">
-            <CssBaseline />
-            <Sheet
-                variant="outlined"
-                sx={{
-                    maxWidth: 400,
-                    mx: 'auto', // margin left & right
-                    my: 4, // margin top & botom
-                    py: 3, // padding top & bottom
-                    px: 2, // padding left & right
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: 2,
-                    borderRadius: 'sm',
-                    boxShadow: 'md',
+        <>
+            <Navbar />
 
-                }}
-            >
-                <Typography level="h4" component="h1">
-                    <b>Bienvenido!</b>
-                </Typography>
-                <Typography level="body2">Inicie sesión o <NavLink to="/Register">registrese</NavLink></Typography>
-
-                <TextField
-                    // html input attribute
-                    name="email"
-                    type="email"
-                    placeholder="johndoe@email.com"
-                    // pass down to FormLabel as children
-                    label="Email"
-                />
-                <TextField
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                    label="Contraseña"
-                />
-
-                <Button
+            <CssVarsProvider className="Ubicacion">
+                <CssBaseline />
+                <Sheet
+                    variant="outlined"
                     sx={{
-                        mt: 1,
-                        bgcolor: '#212121',
-                        '&:hover': {
-                            backgroundColor: '#4e342e',
-                            opacity: [1, 0.9],
-                        }
+                        maxWidth: 400,
+                        mx: 'auto', // margin left & right
+                        my: 4, // margin top & botom
+                        py: 3, // padding top & bottom
+                        px: 2, // padding left & right
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                        borderRadius: 'sm',
+                        boxShadow: 'md',
 
                     }}
-                    className="btnLogin"
                 >
-                    Ingresar
-                </Button>
-                <Typography
-                    endDecorator={<NavLink to="/Register">Registrarse</NavLink>}
-                    fontSize="sm"
-                    sx={{ alignSelf: 'center' }}
-                >
-                    ¿No tienes una cuenta?
+                    <Typography level="h4" component="h1">
+                        <b>Bienvenido!</b>
+                    </Typography>
+                    <Typography level="body2">Inicie sesión o <NavLink to="/Register">registrese</NavLink></Typography>
+                    <form className='mb-3' onSubmit={handleSubmit(onSubmit)}>
+                        <div className='row'>
+                            <div className="col-md-12 mt-3">
+                                <label className="form-label">Email</label>
+                                <input type="email" className="form-control"  aria-invalid={errors.email ? "true" : "false"}{...register("email",{ required: true, maxLength: 25 })}/>
+                            </div>                       
+                            <div className="col-md-12 mt-3">
+                                <label className="form-label">Password</label>
+                                <input type="password" className="form-control" aria-invalid={errors.password ? "true" : "false"}{...register("password",{ required: true, maxLength: 20, minLength: 6 })}/>
+                            </div>
+                        </div>
+                        { loginError.ok == false &&
+                            <p className='text-center text-danger fw-bold mb-0 mt-3'>
+                                Credenciales inválidas...
+                            </p>
+                        }
+                        <div className="position-relative mt-5">
+                            <button
+                                type="submit"
+                                className="btn btn-primary position-absolute top-0 start-50 translate-middle"
+                                >
+                                Ingresar
+                            </button>
+                        </div>
+                    </form>
+                    <Typography
+                        endDecorator={<NavLink to="/Register">Registrarse</NavLink>}
+                        fontSize="sm"
+                        sx={{ alignSelf: 'center' }}
+                    >
+                        ¿No tienes una cuenta?
 
-                </Typography>
-            </Sheet>
+                    </Typography>
+                </Sheet>
 
-        </CssVarsProvider>
+            </CssVarsProvider>
 
+            <Footer />
+        </>
     )
 }
 
